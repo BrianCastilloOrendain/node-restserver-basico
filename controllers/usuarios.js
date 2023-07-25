c405b0b049194c12c6bd1eb3d5b0c5f704d613b4
brian.castillo <brian.castillo@teit.cfe.mx>
2023-07-25 17:18:31 -0600

    First commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,58 @@
+const { response, request } = require('express');
+
+const usuariosGet = (req = request, res = response) => {
+  // const query = req.query; 
+  const { q, nombre = 'No name', apikey, page = 1, limit = 10 } = req.query;
+  res.json({
+    ok: true,
+    msg: 'get API - controlador',
+    q,
+    nombre,
+    apikey,
+    page,
+    limit
+  });
+}
+
+const usuariosPut = (req, res = response) => {
+  const { id } = req.params; 
+  res.json({
+    ok: true,
+    msg: 'put API - controlador',
+    id
+  });
+}
+
+const usuariosPost = (req, res = response) => {
+  // const body = req.body;
+  const { nombre, edad } = req.body;
+  res.json({
+    ok: true,
+    msg: 'post API - controlador',
+    // body
+    nombre,
+    edad
+  });
+}
+
+const usuariosDelete = (req, res = response) => {
+  res.json({
+    ok: true,
+    msg: 'delete API - controlador'
+  });
+}
+
+const usuariosPatch = (req, res = response) => {
+  res.json({
+    ok: true,
+    msg: 'patch API - controlador'
+  });
+}
+
+module.exports = {
+  usuariosGet,
+  usuariosPut,
+  usuariosPost,
+  usuariosDelete,
+  usuariosPatch
+}
